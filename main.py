b5877ff198dc82d79f6c6800b69c7d39d270e13c
sonubintw <sonubintw@gmail.com>
2022-03-10 11:28:33 +0530

    checking if it's updating or not

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("experiment for github and trying to upload for pc")
-print("updated")
+def sort(num):
+    for i in range(5):
+        minP = i
+        for j in range(i, 6):
+            if num[j] < num[minP]:
+                minP = j
+
+        num[i], num[minP] = num[minP], num[i]
+
+        # def reversemkc(num):
+        #     num.reverse()
+        #     return num
+        #print(num)
+
+
+num = [5, 3, 8, 6, 7, 2]
+sort(num)
+print(num)
