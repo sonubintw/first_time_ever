91fe5bbdaaf7006296ab1a11855a196eedc3af93
sonubintw <sonubintw@gmail.com>
2022-03-12 20:28:51 +0530

    pandas and its function

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-def sort(num):
-    for i in range(5):
-        minP = i
-        for j in range(i, 6):
-            if num[j] < num[minP]:
-                minP = j
+import pandas as pd
 
-        num[i], num[minP] = num[minP], num[i]
+#
+# column=["chinuu", "siv", "sivnandana"]
+# a=pd.DataFrame(column)
+# style={"names":column,
+#        "agents":['reyna','phoenix','sage'],
+#        "age":[17,18,19]
+#        }
+# updated_data=pd.DataFrame(style)
+# print(updated_data)
+# select_row=updated_data.iloc[0]["agents"]
+# print(select_row)
+data_crush = {"names": ["siv", "sivnandana", "chinnuuu"],
+              "height": [1.25, 1.55, 1.69],
+              "weight": [56, 55, 60]
 
-        # def reversemkc(num):
-        #     num.reverse()
-        #     return num
-        #print(num)
+              }
+a = pd.DataFrame(data_crush)
+# bmi=weight/height**2
+BMI = []  # empty
+for i in range(len(data_crush)):
+    new = (data_crush["weight"][i]) / (data_crush["height"][i] ** 2)
+    BMI.append(format(new,".2f"))
 
 
-num = [5, 3, 8, 6, 7, 2]
-sort(num)
-print(num)
+a["bmi"]=BMI
+print(a)
+a.to_csv("bmi.txt",sep="\t")
+
+# BMI=pd.DataFrame(data_crush)
+# print(BMI)
