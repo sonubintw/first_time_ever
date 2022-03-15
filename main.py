6a0a236fd4d9c199d2e581358d1031c79f445d6f
sonubintw <sonubintw@gmail.com>
2022-03-15 09:32:50 +0530

    opencv and its function

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-import pandas as pd
+import cv2 as cv
 
-#
-# column=["chinuu", "siv", "sivnandana"]
-# a=pd.DataFrame(column)
-# style={"names":column,
-#        "agents":['reyna','phoenix','sage'],
-#        "age":[17,18,19]
-#        }
-# updated_data=pd.DataFrame(style)
-# print(updated_data)
-# select_row=updated_data.iloc[0]["agents"]
-# print(select_row)
-data_crush = {"names": ["siv", "sivnandana", "chinnuuu"],
-              "height": [1.25, 1.55, 1.69],
-              "weight": [56, 55, 60]
-
-              }
-a = pd.DataFrame(data_crush)
-# bmi=weight/height**2
-BMI = []  # empty
-for i in range(len(data_crush)):
-    new = (data_crush["weight"][i]) / (data_crush["height"][i] ** 2)
-    BMI.append(format(new,".2f"))
+cap=cv.VideoCapture(0)
+cap.set(0,640)#height
+cap.set(0,480)#width
 
 
-a["bmi"]=BMI
-print(a)
-a.to_csv("bmi.txt",sep="\t")
 
-# BMI=pd.DataFrame(data_crush)
-# print(BMI)
+while True:
+
+    rand,vid = cap.read()
+
+    cv.imshow("trial",vid)
+    print(vid)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+
+
